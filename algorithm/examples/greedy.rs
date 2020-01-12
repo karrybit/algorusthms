957a64499e6ea5702811c18e1001acd3a95d8d16
makanai5610 <takumi.k.5610@gmail.com>
2020-01-12 13:59:31 +0900

    impl greedy

--- algorithm/examples/greedy.rs
+++ algorithm/examples/greedy.rs
@@ -4,4 +4,7 @@ fn main() {
     let coins = vec![3, 2, 1, 3, 0, 2];
     let price = 620;
     println!("{}", algorithm::greedy::greedy_pay(coins, price));
+
+    let processes = vec![(1, 3), (2, 5), (4, 7), (6, 9), (8, 10)];
+    println!("{}", algorithm::greedy::range_scheduling(processes.clone()));
 }
--- algorithm/src/greedy.rs
+++ algorithm/src/greedy.rs
@@ -8,3 +8,17 @@ pub fn greedy_pay(coins: Vec<u32>, mut price: u32) -> u32 {
     });
     count
 }
+
+pub fn range_scheduling(mut processes: Vec<(u32, u32)>) -> u32 {
+    processes.sort_by(|p1, p2| p1.1.cmp(&p2.1));
+    let mut count = 0;
+    let mut time = 0;
+    for process in processes.iter() {
+        if process.0 < time {
+            continue;
+        }
+        count += 1;
+        time = process.1;
+    }
+    count
+}
