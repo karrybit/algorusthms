8eb943bd4bc37e96f92a89f146b44dea8bde4619
makanai5610 <takumi.k.5610@gmail.com>
2020-01-28 13:44:06 +0900

    impl memo

--- algorithm/examples/dp.rs
+++ algorithm/examples/dp.rs
@@ -2,9 +2,18 @@ use algorithm;
 
 fn main() {
     greedy();
+    memo();
 }
 
 fn greedy() {
     let input = vec![(2, 3), (1, 2), (3, 4), (2, 2)];
-    assert_eq!(7, algorithm::dp::greedy(&input, 0, 5));
+    let ans = algorithm::dp::greedy(&input, 0, 5);
+    assert_eq!(7, ans);
+}
+
+fn memo() {
+    let input = vec![(2, 3), (1, 2), (3, 4), (2, 2)];
+    let mut table: Vec<Vec<i32>> = vec![vec![-1; input.len()]; input.len()];
+    let ans = algorithm::dp::memo(&mut table, &input, 0, 5);
+    assert_eq!(7, ans);
 }
--- algorithm/src/dp.rs
+++ algorithm/src/dp.rs
@@ -8,3 +8,18 @@ pub fn greedy(input: &[(u32, u32)], index: usize, weight: u32) -> u32 {
         ),
     }
 }
+
+pub fn memo(table: &mut Vec<Vec<i32>>, input: &[(i32, i32)], index: usize, weight: i32) -> i32 {
+    if table[index][weight as usize] >= 0 {
+        return table[index][weight as usize];
+    };
+    table[index][weight as usize] = match input.get(index) {
+        None => 0,
+        Some((w, _)) if weight < *w => memo(table, input, index + 1, weight),
+        Some((w, v)) => std::cmp::max(
+            memo(table, input, index + 1, weight),
+            memo(table, input, index + 1, weight - w) + v,
+        ),
+    };
+    table[index][weight as usize]
+}
