81a2b88e831b76ba72bccaf01396aa9d3c44d488
makanai5610 <takumi.k.5610@gmail.com>
2020-01-30 13:32:33 +0900

    add  dp

--- algorithm/examples/dp.rs
+++ algorithm/examples/dp.rs
@@ -3,17 +3,30 @@ use algorithm;
 fn main() {
     greedy();
     memo();
+    dp();
 }
 
 fn greedy() {
+    println!("--- greedy ---");
     let input = vec![(2, 3), (1, 2), (3, 4), (2, 2)];
     let ans = algorithm::dp::greedy(&input, 0, 5);
-    assert_eq!(7, ans);
+    println!("{}", ans);
 }
 
 fn memo() {
+    println!("--- memo ---");
     let input = vec![(2, 3), (1, 2), (3, 4), (2, 2)];
-    let mut table: Vec<Vec<i32>> = vec![vec![-1; input.len()]; input.len()];
-    let ans = algorithm::dp::memo(&mut table, &input, 0, 5);
-    assert_eq!(7, ans);
+    let weight = 5;
+    let mut table: Vec<Vec<Option<i32>>> = vec![vec![None; weight + 1]; input.len() + 1];
+    let ans = algorithm::dp::memo(&mut table, &input, 0, weight);
+    println!("{}", ans.unwrap());
+}
+
+fn dp() {
+    println!("--- dp ---");
+    let input = vec![(2, 3), (1, 2), (3, 4), (2, 2)];
+    let weight = 5;
+    let mut table: Vec<Vec<i32>> = vec![vec![0; weight + 1]; input.len() + 1];
+    let ans = algorithm::dp::dp(&mut table, &input, weight);
+    println!("{}", ans);
 }
--- algorithm/src/dp.rs
+++ algorithm/src/dp.rs
@@ -1,4 +1,5 @@
 pub fn greedy(input: &[(u32, u32)], index: usize, weight: u32) -> u32 {
+    println!("index: {}, weight: {}", index, weight);
     match input.get(index) {
         None => 0,
         Some((w, _)) if weight < *w => greedy(input, index + 1, weight),
@@ -9,17 +10,51 @@ pub fn greedy(input: &[(u32, u32)], index: usize, weight: u32) -> u32 {
     }
 }
 
-pub fn memo(table: &mut Vec<Vec<i32>>, input: &[(i32, i32)], index: usize, weight: i32) -> i32 {
-    if table[index][weight as usize] >= 0 {
-        return table[index][weight as usize];
+pub fn memo(
+    table: &mut Vec<Vec<Option<i32>>>,
+    input: &[(i32, i32)],
+    index: usize,
+    weight: usize,
+) -> Option<i32> {
+    println!("table: {:?}", table);
+    println!("index: {}, weight: {}", index, weight);
+    if let Some(value) = table.get(index).and_then(|_table| _table.get(weight)) {
+        if value.map_or(false, |value| value >= 0) {
+            return *value;
+        }
+    }
+
+    table[index][weight] = if index == input.len() {
+        Some(0)
+    } else {
+        match input.get(index) {
+            None => Some(0),
+            Some((w, _)) if weight < *w as usize => memo(table, input, index + 1, weight),
+            Some((w, v)) => std::cmp::max(
+                memo(table, input, index + 1, weight),
+                memo(table, input, index + 1, weight - *w as usize).map(|value| value + v),
+            ),
+        }
     };
-    table[index][weight as usize] = match input.get(index) {
-        None => 0,
-        Some((w, _)) if weight < *w => memo(table, input, index + 1, weight),
-        Some((w, v)) => std::cmp::max(
-            memo(table, input, index + 1, weight),
-            memo(table, input, index + 1, weight - w) + v,
-        ),
-    };
-    table[index][weight as usize]
+    table[index][weight]
+}
+
+pub fn dp(table: &mut Vec<Vec<i32>>, input: &[(i32, i32)], weight: usize) -> i32 {
+    println!("{:?}", table);
+    (0..input.len()).for_each(|i| {
+        (0..=weight).for_each(|j| {
+            println!(
+                "input[{}].value: {}, input[{}].weight: {}, j: {}",
+                i, input[i].1, j, input[i].0, j
+            );
+            table[i + 1][j] = if j < input[i].0 as usize {
+                table[i][j]
+            } else {
+                std::cmp::max(table[i][j], table[i][j - input[i].0 as usize] + input[i].1)
+            };
+            println!("{:?}", table);
+        })
+    });
+    println!("{:?}", table);
+    table[input.len()][weight]
 }
